4bd5915794ea96994d336641deb63752086f6b8f
rsingh3mags <135671947+rsingh3mags@users.noreply.github.com>
2024-07-31 22:43:04 +1200

    Can collect gems and is displayed on HUD

--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
+#adding the coins once they are collected
+func _ready():
+	for coins in $Coins.get_children():
+		coins.coins_collected.connect(_on_coins_collected)
 
-
+#When player enters the door change to "level_2"
 func _on_area_3d_body_entered(body):
 	if body ==$Player:
 		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
