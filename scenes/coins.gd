f1eafbf25fdd450c8a6b6947822f23d73dc104a0
rsingh3mags <135671947+rsingh3mags@users.noreply.github.com>
2024-07-31 22:38:38 +1200

    Added HUD For level display

--- scenes/coins.gd
+++ scenes/coins.gd
@@ -1,7 +1,11 @@
 extends Area3D
 
+#signals to global
 signal coins_collected
 
+#when player enters remove coin
 func _on_body_entered(body):
 	if body.name == "Player":
+		Global.coins_collected += 1
+		coins_collected.emit()
 		queue_free()
--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -5,3 +5,7 @@ extends Node3D
 func _on_area_3d_body_entered(body):
 	if body ==$Player:
 		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+
+func _on_coins_collected():
+	$HUD/CoinsLabel.text = "Coins: " +  str(Global.coins_collected)
