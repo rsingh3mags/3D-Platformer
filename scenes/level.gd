1ea4f4cd3acf0ce2001c4c25b9bb5f80b49e66e2
rsingh3mags <135671947+rsingh3mags@users.noreply.github.com>
2024-08-12 20:35:04 +1200

    added background fixed coins and made a better world envoirnment.

--- scenes/level.gd
+++ scenes/level.gd
@@ -5,12 +5,15 @@ extends Node3D
 #adding the coins once they are collected
 func _ready():
 	print("level " + str(level_num) + " ready!")
+	set_coins_label()
 	for coins in $Coins.get_children():
 		coins.coins_collected.connect(_on_coins_collected)
 
 #when coins collected
 func _on_coins_collected():
-	$HUD/CoinsLabel.text = "Coins: " +  str(Global.coins_collected)
+	set_coins_label()
 	
+func set_coins_label():
+	$HUD/CoinsLabel.text = "Coins: " +  str(Global.coins_collected)
 func _on_door_player_entered(level):
 	get_tree().change_scene_to_file(level)
